48f3d52c0374aff55ca3f8b735cd5dc2dbb48243
impassionate <46687271+impassionate@users.noreply.github.com>
2022-12-07 22:48:04 -0600

    Add files via upload

---
+++ project/config/config.default.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var config = require("./config.webgme"),
+  validateConfig = require("webgme/config/validator");
+
+// Add/overwrite any additional settings here
+config.server.port = 8888;
+
+//allowing python plugin execution
+config.plugin.allowServerExecution = true;
+
+// Paths needed for proper jointjs import on the client side
+config.requirejsPaths["jointjs"] = "./node_modules/jointjs/dist/joint.min";
+config.requirejsPaths["lodash"] = "./node_modules/lodash/lodash.min";
+
+validateConfig(config);
+module.exports = config;
